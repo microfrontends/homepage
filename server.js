c30837c5a88a20f022243c8edaaefe341bf92f69
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-24 00:45:45 -0300

    Alternative 3: Client-Side JavaScript

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const server = express();
 const request = require('request');
+const proxy = require('http-proxy-middleware');
 
 server.set('view engine', 'ejs');
+
+const createProxy = (path, target) =>
+  server.use(path, proxy({ target, changeOrigin: true, ignorePath: true }));
+
+createProxy('/header', 'https://microfrontends-header.herokuapp.com/');
+createProxy('/products-list', 'https://microfrontends-products-list.herokuapp.com/');
+createProxy('/cart', 'https://microfrontends-cart.herokuapp.com/');
 
 server.get('/', (req, res) => res.render('index'));
 
