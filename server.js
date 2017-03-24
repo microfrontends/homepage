39dcbe8ff6a7ac7285be1cc9f2b35d35760cb6ed
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-24 01:53:47 -0300

    Alternative 4: Progressive loading from the back-end

--- server.js
+++ server.js
@@ -1,18 +1,59 @@
 const express = require('express');
 const server = express();
 const request = require('request');
-const proxy = require('http-proxy-middleware');
+const ejs = require('ejs');
+const padRight = require('pad-right');
 
 server.set('view engine', 'ejs');
 
-const createProxy = (path, target) =>
-  server.use(path, proxy({ target, changeOrigin: true, ignorePath: true }));
+server.get('/', (req, res) => {
+  let appsToLoad = [
+    { name: 'header', url: 'https://microfrontends-header.herokuapp.com/' },
+    { name: 'productsList', url: 'https://microfrontends-products-list.herokuapp.com/' },
+    { name: 'cart', url: 'https://microfrontends-cart.herokuapp.com/' }
+  ]
+  let previousFlushedData = '';
 
-createProxy('/header', 'https://microfrontends-header.herokuapp.com/');
-createProxy('/products-list', 'https://microfrontends-products-list.herokuapp.com/');
-createProxy('/cart', 'https://microfrontends-cart.herokuapp.com/');
+  const load = (piece, index) =>
+    getContents(piece.url).then(data => {
+      appsToLoad[index].data = data;
+      flushData();
+    });
 
-server.get('/', (req, res) => res.render('index'));
+  const buildDataForEjs = () =>
+    appsToLoad.reduce((params, app) => {
+      params[app.name] = app.data || 'PENDING';
+      return params;
+    }, {});
+
+  const flushData = () => {
+    const dataForEjs = buildDataForEjs();
+    ejs.renderFile('views/index.ejs', dataForEjs, {}, (err, renderedHtml) => {
+      if (err) throw err;
+
+      const htmlToBeFlushed = renderedHtml.split('PENDING')[0].replace(previousFlushedData, '');
+
+      // chunks have to have at least 4096 bytes to make sure browsers print them
+      res.write(padRight(htmlToBeFlushed, 4096, ' '));
+      previousFlushedData += htmlToBeFlushed;
+    });
+  }
+
+  flushData();
+  Promise.all(
+    appsToLoad.map(load)
+  ).then(() =>
+    res.end()
+  );
+});
+
+const getContents = (url) => new Promise((resolve, reject) => {
+  request.get(url, (error, response, body) => {
+    if (error) return resolve("Error loading " + url + ": " + error.message);
+
+    return resolve(body);
+  });
+});
 
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
