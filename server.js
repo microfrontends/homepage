b5a69b2e81b3f2b7b115eed7227b98d82cd6f063
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-26 01:48:43 -0300

    Point proxies to router

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const server = express();
-const request = require('request');
 const proxy = require('http-proxy-middleware');
 
 server.set('view engine', 'ejs');
 
-const createProxy = (path, target) =>
-  server.use(path, proxy({ target, changeOrigin: true, pathRewrite: {[`^${path}`]: ''} }));
-
-createProxy('/header', 'https://microfrontends-header.herokuapp.com/');
-createProxy('/products-list', 'https://microfrontends-products-list.herokuapp.com/');
-createProxy('/cart', 'https://microfrontends-cart.herokuapp.com/');
-
 server.get('/', (req, res) => res.render('index'));
+
+server.use('*', proxy({
+  target: 'https://microfrontends.herokuapp.com',
+  changeOrigin: true
+}));
 
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
